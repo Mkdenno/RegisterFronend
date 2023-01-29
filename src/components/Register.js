bb7fea78d6ce5b57f9a9f491cef258e648043944
denniskiprop25@gmail.com <dennis kiprop masiror>
2023-01-29 09:00:52 +0300

    It will add Post to register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,39 +1,119 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    const inputData={
+      username,
+      email,
+      password,
+      password_confirmation: passwordConfirmation
+    }
+
+    console.log(inputData)
+
+    fetch("http://localhost:3000/signup", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        user: inputData,
+      }),
+    }).then((response) => {
+
+      if (response.ok) {
+
+        response.json().then((user) => {
+          console.log(user)
+        });
+        navigate("/login");
+      } else {
+        response.json().then((errorData) => {
+          console.log(errorData)
+
+          setErrors(errorData.errors)
+        });
+      }
+    });
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    setPasswordConfirmation("");
+  };
+  
+
   return (
-    <div className='  grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-      <div className='hidden sm:block'>
-      <img src="./images/register.gif" alt="GIF" />
+    <div className="  grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <div className="hidden sm:block">
+        <img src="./images/register.gif" alt="GIF" />
       </div>
-      <div className='flex flex-col justify-center'>
-        <form className='max-w-[400px] w-full mx-auto p-8 shadow-2xl px-8 rounded-lg'>
-          <h2 className='text-4xl font-bold text-center'>Sign Up</h2>
-          <div className='flex flex-col text-gray-400 py-2'>
-            <label className=''>Username</label>
-            <input type='text' name='username' className='rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none'/>
+      <div className="flex flex-col justify-center">
+        <form onSubmit={handleSubmit} className="max-w-[400px] w-full mx-auto p-8 shadow-2xl px-8 rounded-lg">
+          <h2 className="text-4xl font-bold text-center">Sign Up</h2>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className="">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={((e)=>setUsername(e.target.value))}
+              className="rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none"
+            />
           </div>
-          <div className='flex flex-col text-gray-400 py-2'>
-            <label className=''>Email</label>
-            <input type='text' name='username' className='rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none'/>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className="">Email</label>
+            <input
+              type="email"
+              name="username"
+              value={email}
+              onChange={((e)=>setEmail(e.target.value))}
+              className="rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none"
+            />
           </div>
-          <div className='flex flex-col text-gray-400 py-2'>
-            <label className=''>Password</label>
-            <input type='password' name='username' className='rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none '/>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className="">Password</label>
+            <input
+              type="password"
+              name="username"
+              value={password}
+              onChange={((e)=>setPassword(e.target.value))}
+              className="rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none "
+            />
           </div>
-          <div className='flex flex-col text-gray-400 py-2'>
-            <label className=''> Confirm Password</label>
-            <input type='password' name='username' className='rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none '/>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className=""> Confirm Password</label>
+            <input
+              type="password"
+              name="username"
+              value={passwordConfirmation}
+              onChange={((e)=>setPasswordConfirmation(e.target.value))}
+              className="rounded-lg bg-gray-200 mt-2  p-2 focus:border-blue-500 focus:outline-none "
+            />
           </div>
-          <div className='flex justify-between text-gray-40 py-2'>
+          <div className="flex justify-between text-gray-40 py-2">
             <p>Already have account?</p>
-            <p>Login</p>
+            <Link to="/login"><p>Login</p></Link>
           </div>
-          <button className='w-full my-5 bg-blue-400 shadow-lg shadow-blue-500/50 hover:shadow-blue-400/30 text-white rounded-lg p-2'>Sign Up</button>
+          <button className="w-full my-5 bg-blue-400 shadow-lg shadow-blue-500/50 hover:shadow-blue-400/30 text-white rounded-lg p-2">
+            Sign Up
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
